ee116ff77d249dbec5f378f308c76b94dfbf5dc8
lartweib <a.federicodaniel@gmail.com>
2022-09-14 17:45:39 +0200

    modificar metodo get en vistas basadas en clases

--- core/views.py
+++ core/views.py
@@ -5,11 +5,8 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     template_name = "core/home.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = "Mi web Playground"
-        return context
-
+    def get(self, request, *args, **kargs):
+        return render(request, self.template_name, {'title':"Mi web playground"})
 
 
 class SamplePageView(TemplateView):
