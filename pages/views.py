0618e5e71e7520ff90877f955d8e6a6401e0584a
lartweib <a.federicodaniel@gmail.com>
2022-09-15 17:01:58 +0200

    reemplazar sentencias del mixin por decorador

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import redirect
 from .models import Page
@@ -12,9 +14,9 @@ class StaffRequiredMixin(object):
     Este mixin requerira que el usuario sea miembro del staff
     """
 
+    @method_decorator(staff_member_required)
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return redirect(reverse_lazy('admin:login'))
+
         return super(PageCreate, self).dispatch(request, *args,**kwargs)
 
 # Create your views here.
