9790033ee02d3128db98675e8b1b0aa43d438705
lartweib <a.federicodaniel@gmail.com>
2022-09-15 19:25:00 +0200

    acomodando la estetica del registro

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-
+from django import forms
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
@@ -10,3 +10,11 @@ class SignUpView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('login') + '?register'
+
+    def get_form(self, form_class=None):
+        form = super(SignUpView, self).get_form()
+        # Modificar en tiempo real 
+        form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2', 'placeholder':'Nombre de usuario'})
+        form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Contraseña'})
+        form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Repite la contraseña'})
+        return form
