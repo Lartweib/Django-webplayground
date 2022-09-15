c7c862ab27fae4d7b2b0d5454dfae8a54f1bfd62
lartweib <a.federicodaniel@gmail.com>
2022-09-15 16:45:16 +0200

    formularios para modelos en cbv

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Ck editor
+
+CKEDITOR_CONFIGS = {
+                    'default': {'width': 'auto',},
+                    }
