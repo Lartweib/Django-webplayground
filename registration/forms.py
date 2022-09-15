b8d7f41aa95419871b88b0d76bcf7cc1ffb39529
lartweib <a.federicodaniel@gmail.com>
2022-09-15 20:32:04 +0200

    validar email unico

--- registration/forms.py
+++ registration/forms.py
@@ -11,4 +11,8 @@ class UserCreationFormWithEmail(UserCreationForm):
         model = User 
         fields = ("username","email","password1","password2")
 
-        
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("El email ya esta registrado.")
+        return email
