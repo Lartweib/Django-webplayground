044d629c726ecd3035f7a308ffc0bd1c2068f41a
lartweib <a.federicodaniel@gmail.com>
2022-09-16 18:44:33 +0200

    opcion editar email

--- registration/urls.py
+++ registration/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from .views import ProfileUpdate, SignUpView 
+from .views import ProfileUpdate, SignUpView, EmailUpdate
 
 urlpatterns = [
     path('signup/',SignUpView.as_view(), name="signup"),
     path('profile/',ProfileUpdate.as_view(), name="profile"),
+    path('profile/email/',EmailUpdate.as_view(), name="profile_email"),
 
 ]
