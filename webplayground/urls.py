25c94f5c2d69f42aa9fccb25acb44244745f001a
lartweib <a.federicodaniel@gmail.com>
2022-09-16 21:08:31 +0200

    perfiles publicos app profiles

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from pages.urls import pages_patterns
 from django.conf import settings
-
+from pages.urls import pages_patterns
+from profiles.urls import profiles_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
@@ -11,6 +11,7 @@ urlpatterns = [
     # Paths del auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
+    path('profiles/',include(profiles_patterns)),
 ]
 
 if settings.DEBUG:
