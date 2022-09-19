e23352764441596f71ced55a9ca691a784087806
lartweib <a.federicodaniel@gmail.com>
2022-09-19 16:10:00 +0200

    ordenar las conversaciones en messenger

--- messenger/models.py
+++ messenger/models.py
@@ -28,8 +28,12 @@ class ThreadManager(models.Manager):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
 
     objects = ThreadManager()
+
+    class Meta:
+        ordering = ['-updated']
 
 def messages_changed(sender, **kwargs):
     instance = kwargs.pop("instance",None)
@@ -48,5 +52,7 @@ def messages_changed(sender, **kwargs):
     # Buscar los mensajes de false_pk_set que si estan en pk_set y los borramos de pk_set
     pk_set.difference_update(false_pk_set)
 
+    # Forzar la actualizacion haciendo save
+    instance.save()
 
 m2m_changed.connect(messages_changed,sender=Thread.messages.through)
